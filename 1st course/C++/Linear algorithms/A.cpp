3d38648fd94c47e4371e414821f4331f02ec75c4
pigsel7 <111083053+pigsel7@users.noreply.github.com>
2023-02-22 12:02:00 +0300

    Create A.cpp

---
+++ 1st course/C++/Linear algorithms/A.cpp
@@ -0,0 +1,12 @@
+int main(){
+    
+    ll a, b;
+    
+    scanf("%lld%lld", &a, &b);
+    printf("%lld\n", a + b);
+    printf("%lld\n", a - b);
+    printf("%lld\n", a * b);
+    printf("%.3f\n", (double)a / b);
+        
+    return 0;
+}
