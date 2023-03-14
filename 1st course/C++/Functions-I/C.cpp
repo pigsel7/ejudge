8da2f14e20ad1a4d4bf078eb5c7bbe45069f5134
pigsel7 <111083053+pigsel7@users.noreply.github.com>
2023-03-14 20:23:36 +0300

    Update C.cpp

--- 1st course/C++/Functions-I/C.cpp
+++ 1st course/C++/Functions-I/C.cpp
@@ -1,45 +1,35 @@
-void remove(vector<int> &a, int k, bool left){
-    int n = (int)a.size();
+void remove(int *a, int n, int k, bool left){
     
     if(left){
-        for(int i = k; i < n; i++){
-            printf("%d ", a[i]);
-        }
-        for(int i = 0; i < k; i++){
-            printf("%d ", a[i]);
-        }
-    }
-    else{
-        for(int i = n - k; i < n; i++){
-            printf("%d ", a[i]);
-        }
-        for(int i = 0; i < n - k; i++){
-            printf("%d ", a[i]);
-        }
+        while(k--) for(int i = 0; i < n - 1; i++) swap(a[i], a[i + 1]);
+        return;
     }
     
-    printf("\n");
+    while(k--) for(int i = n - 1; i > 0; i--) swap(a[i], a[i - 1]);
 }
 
 char direction[100];
+int a[1001];
+
 
 int main(){
     
     int n, k;
     scanf("%d", &n);
     
-    vector<int> a(n);
     
-    for(int i = 0; i < n; i++){
-        scanf("%d", &a[i]);
-    }
+    for(int i = 0; i < n; i++) scanf("%d", &a[i]);
     
     scanf("%d%s", &k, direction);
     
     bool dir = true;
     if(direction[0] == 'r') dir = false;
     
-    remove(a, k, dir);
+    remove(a, n, k, dir);
+    
+    for(int i = 0; i < n; i++) printf("%d ", a[i]);
+    
+    printf("\n");
     
     
     return 0;
