6ff570bfcef9b10616a91e698f4f60bc9af96164
pigsel7 <111083053+pigsel7@users.noreply.github.com>
2023-03-14 20:11:57 +0300

    Update G.cpp

--- 1st course/C++/Functions-I/G.cpp
+++ 1st course/C++/Functions-I/G.cpp
@@ -1,14 +1,21 @@
-int main() {
-    
-    double n;
-    scanf("%lf", &n);
-    
+double f(double n){
     double ans = 1;
     double d = 2;
     
     for(int i = 1; i * 2 + 1 < n; i++){
         ans *= (n / 2 - (i + d++) / 2) / (n / 2 - i);
     }
+    
+    return ans;
+}
+
+
+int main() {
+    
+    double n;
+    scanf("%lf", &n);
+    
+    double ans = f(n);
     
     printf("%.4lf\n", ans);
         
