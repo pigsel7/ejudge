28f3ddd5c9f2ef98ffa09bb54f959458e6cdd6d9
pigsel7 <111083053+pigsel7@users.noreply.github.com>
2023-03-30 13:09:24 +0300

    Create B.cpp

---
+++ 1st course/C++/Other problems/B.cpp
@@ -0,0 +1,13 @@
+int main(){
+    
+    ll number, answer = 0;
+    
+    while(scanf("%lld", &number) != EOF){
+        answer += number;
+    }
+    
+    printf("%lld\n", answer);
+    
+    
+    return 0;
+}
