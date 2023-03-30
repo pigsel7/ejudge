67ac156f6414023c72ddff7aa3a445e1b770049e
pigsel7 <111083053+pigsel7@users.noreply.github.com>
2023-03-30 13:09:03 +0300

    Create A.cpp

---
+++ 1st course/C++/Other problems/A.cpp
@@ -0,0 +1,9 @@
+int main(){
+    
+    ll firstNumber, secondNumber;
+    
+    scanf("%lld%lld", &firstNumber, &secondNumber);
+    printf("%lld\n", firstNumber + secondNumber);
+    
+    return 0;
+}
