0a28c133255ba6024822aed5aa43f4a7ab3bb493
pigsel7 <111083053+pigsel7@users.noreply.github.com>
2023-06-08 15:45:53 +0300

    Create BD.cpp

---
+++ 1st course/C++/Other problems/BD.cpp
@@ -0,0 +1,13 @@
+int main(){
+    
+    int t;
+    scanf("%d", &t);
+    
+    while(t--){
+        int a, b;
+        scanf("%d%d", &a, &b);
+        printf("%d\n", a * b);
+    }
+    
+    return 0;
+}
